a3a6e4428436db0c9c03671b1f13fc8c1ee6b5a3
Carlos Rene Perez G <d.londocreed@gmail.com>
2024-11-25 17:41:17 -0600

    retoque

---
+++ controllers/auth/signUp.js
@@ -0,0 +1,32 @@
+import User from '../../models/User.js'
+
+let SignUp = async (req, res, next) => {
+    try {
+        let user = req.body;
+        user.online = true;
+
+        let newUser = await User.create(user);
+
+        return res.status(201).json({
+            success: true,
+            response: {
+                user: {
+                    email: newUser.email,
+                    name: newUser.name, 
+                    lastname: newUser.lastname,
+                    photo: newUser.photo,
+                    country: newUser.country,
+                    online: true
+                },
+                token: req.token
+            },
+            message: 'Usuario registrado exitosamente'
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+
+export default SignUp;
--- router/auth.js
+++ router/auth.js
@@ -4,13 +4,20 @@ import signOut from "../controllers/auth/signOut.js";
 import passport from "../middlewares/passport.js";
 import passportGoogle from "../middlewares/passportGoogle.js";
 import accountNoExists  from "../middlewares/accountNoExists.js";
-import  generateToken  from "../middlewares/generateToken.js";
+import generateToken  from "../middlewares/generateToken.js";
 import isValidatedPassword from "../middlewares/isValidatedPassword.js";
 import googleCallback from "../controllers/auth/googleCallback.js";
+import validator from "../middlewares/validator.js";
+import userSchema from "../schemas/userSchema.js"
+import accountExists from "../middlewares/accountExist.js";
+import createHash from "../middlewares/createHash.js";
+import SignUp from "../controllers/auth/signUp.js";
+
 
 const routerAuth = Router();
 routerAuth.post('/signIn', accountNoExists, isValidatedPassword, generateToken, signIn);
 routerAuth.post('/signOut', passport.authenticate('jwt', { session: false }), signOut);
+routerAuth.post('/signUp',validator(userSchema),accountExists,createHash, generateToken, SignUp)
 
 routerAuth.get('/signin/google', passportGoogle.authenticate('google', { session: false, scope: ['profile', 'email'] }));
 
